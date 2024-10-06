20cc445fda5fe5752e1a96881d9585d49a3fa9aa
alja667 <aprogozhina@gmail.com>
2024-10-06 21:42:54 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,17 @@
+from fake_math import divide as fake_divide
+from truee_math import divide as tru_divide
+
+fake_divide
+
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+print(result1)
+print(result2)
+
+tru_divide
+
+result3 = tru_divide(49, 7)
+result4 = tru_divide(15, 0)
+
+print(result3)
+print(result4)
